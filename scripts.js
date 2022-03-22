0adb5347cc1ad3b8537b97dbe9eccdfabf9342d4
Michael Guedko <mjgeko@gmail.com>
2022-03-22 14:48:30 -0400

    Reformat Gameboard data & add TODO

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ const DisplayController = (() => {
         tiles.forEach(element => {
             element.addEventListener('click', (e) => {
                 fill(e);
-                Gameboard.play(e.target.id);
+                Gameboard.play(e.target.id, true);
                 Gameboard.log();
             });
         });
@@ -49,20 +49,27 @@ const DisplayController = (() => {
 })();
 
 const Gameboard = (() => {
-    let data = [0, 0, 0,
-                0, 0, 0,
-                0, 0, 0];
+    let data = ['0', '0', '0',
+                '0', '0', '0',
+                '0', '0', '0'];
 
     let round = 0;
 
-    const play = (tileID) => {
-        data[tileID] = 1;
-        round++;
+    const play = (tileID,human) => {
+        if(human){
+            data[tileID] = human.getMarker;
+            round++;
+        }
+        else{
+            data[tileID] = human.getAiMarker;
+            round++;
+        }
+        
     };
     const clear = () => {
-        data = [0, 0, 0,
-                0, 0, 0,
-                0, 0, 0];
+        data = ['0', '0', '0',
+                '0', '0', '0',
+                '0', '0', '0'];
 
         round = 0;
         console.log('clear');
@@ -79,21 +86,25 @@ const Gameboard = (() => {
         log
     }
 })();
-
+//TODO: rewrite this so you have one player is a human and the other is the AI
 const Player = () => {
     let marker = 'X';
+    let aiMarker = 'O';
     const setMarker = (bool) => {
         if (bool.target.checked) {
-            marker = 'O'
+            marker = 'O';
+            aiMarker = 'X';
         } else {
+            aiMarker = 'O';
             marker = 'X';
         }
     }
     const getMarker = () => marker;
-
+    const getAiMarker = () => aiMarker;
     return {
         setMarker,
-        getMarker
+        getMarker,
+        getAiMarker
     }
 };
 
